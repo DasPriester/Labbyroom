2a62e25b510629d45242779de77e7acee6cd2770
Niklas267 <unrau73.wu@gmail.com>
2022-04-27 16:19:28 +0200

    Player Movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController cc;
+
+    public float speed = 5f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    
+    bool isGrounded;
+
+    Vector3 vel;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance);
+
+        if (isGrounded && vel.y < 0)
+            vel.y = -2f;
+
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        cc.Move(speed * Time.deltaTime * move);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            vel.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        vel.y += gravity * Time.deltaTime;
+
+        cc.Move(vel * Time.deltaTime);
+    }
+}
