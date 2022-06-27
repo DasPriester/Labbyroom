f4d7fb5daa2a7501adfe15bb75fe752b85025049
DasPriester <michelrothboeck@gmail.com>
2022-06-27 05:28:25 +0200

    more UI shit

--- Assets/Editor/SerializableDictionaryEditor.cs
+++ Assets/Editor/SerializableDictionaryEditor.cs
@@ -5,17 +5,17 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
-[CustomPropertyDrawer(typeof(SerializableDictionary<PickUpInteractable, int>))]
-class SerializableDictionaryEditor : PropertyDrawer
+abstract class SerializableDictionaryEditor : PropertyDrawer
 {
-    private PickUpInteractable[] _implementations;
-
+    protected UnityEngine.Object[] _implementations_key;
+    protected UnityEngine.Object[] _implementations_value;
+    protected List<string> used_keys;
+    protected List<string> used_values;
     public static GUILayoutOption miniButtonWidth = GUILayout.Width(20f);
-
     private static GUIContent
          moveButtonContent = new GUIContent("\u21b4", "move down"),
          //duplicateButtonContent = new GUIContent("+", "duplicate"),
-         deleteButtonContent = new GUIContent("-", "delete");
+         deleteButtonContent = new GUIContent("x", "delete");
 
     private static void ShowButtons(SerializedProperty keys, SerializedProperty values, int index)
     {
@@ -30,65 +30,68 @@ class SerializableDictionaryEditor : PropertyDrawer
         }
     }
 
-    public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
         label = EditorGUI.BeginProperty(position, label, property);
-		Rect contentPosition = EditorGUI.PrefixLabel(position, label);
+        Rect contentPosition = EditorGUI.PrefixLabel(position, label);
 
         SerializedProperty keys = property.FindPropertyRelative("keys");
         SerializedProperty values = property.FindPropertyRelative("values");
+
+        used_keys = new List<string>();
+        used_values = new List<string>();
 
         EditorGUI.indentLevel += 1;
 
         GUILayout.BeginHorizontal();
 
-        if (_implementations == null)
+        if (searchPathKey() != "")
         {
-            _implementations = Resources.LoadAll<PickUpInteractable>("Prefabs");
+            if (_implementations_key == null)
+            {
+                _implementations_key = Resources.LoadAll(searchPathKey());
+            }
+
+            EditorGUILayout.LabelField($"Found {_implementations_key.Count()} " + searchPathKey().ToLower(), GUILayout.MinWidth(80f));
+
+            if (GUILayout.Button(new GUIContent("\u21ba", "Refresh " + searchPathKey().ToLower()), GUILayout.Width(40f)))
+            {
+                _implementations_key = Resources.LoadAll(searchPathKey());
+            }
+
+            for (int x = 0; x < keys.arraySize; x++)
+            {
+                used_keys.Add(keys.GetArrayElementAtIndex(x).objectReferenceValue.name);
+            }
         }
 
-        EditorGUILayout.LabelField($"Found {_implementations.Count()} items");
-
-        if (GUILayout.Button("Refresh items"))
+        if (searchPathValue() != "")
         {
-            _implementations = Resources.LoadAll<PickUpInteractable>("Prefabs");
+            if (_implementations_value == null)
+            {
+                _implementations_value = Resources.LoadAll(searchPathValue());
+            }
+
+            EditorGUILayout.LabelField($"Found {_implementations_key.Count()} " + searchPathValue().ToLower(), GUILayout.MinWidth(80f));
+
+            if (GUILayout.Button(new GUIContent("\u21ba", "Refresh " + searchPathValue().ToLower()), GUILayout.Width(40f)))
+            {
+                _implementations_value = Resources.LoadAll(searchPathValue());
+            }
+
+            for (int x = 0; x < values.arraySize; x++)
+            {
+                used_values.Add(values.GetArrayElementAtIndex(x).objectReferenceValue.name);
+            }
         }
 
         GUILayout.EndHorizontal();
 
-        List<string> used = new List<string>();
-
-        for (int x = 0; x < keys.arraySize; x++)
-        {
-            used.Add(keys.GetArrayElementAtIndex(x).objectReferenceValue.name);
-        }
-
-        Debug.Log(keys.arraySize);
         for (int i = 0; i < keys.arraySize; i++)
         {
             GUILayout.BeginHorizontal();
 
-            EditorGUILayout.PropertyField(values.GetArrayElementAtIndex(i), GUIContent.none, GUILayout.Width(40f));
-
-            EditorGUILayout.LabelField("x", GUILayout.Width(30f));
-
-            string[] names = _implementations.Select(impl => impl.name).ToArray();
-            string oname = keys.GetArrayElementAtIndex(i).objectReferenceValue.name;
-            int _implementationTypeIndex = 0;
-
-            for (int x = 0; x < names.Length; x++)
-            {
-                if (names[x] == oname)
-                    _implementationTypeIndex = x;
-            }
-
-            int temp_implementationTypeIndex = EditorGUILayout.Popup(GUIContent.none,
-                _implementationTypeIndex, names);
-
-            string str = _implementations[temp_implementationTypeIndex].name;
-            if (!used.Contains(str) || keys.GetArrayElementAtIndex(i).objectReferenceValue.name == str) {
-                _implementationTypeIndex = temp_implementationTypeIndex;
-                keys.GetArrayElementAtIndex(i).objectReferenceValue = _implementations[_implementationTypeIndex];
-            }
+            ListField(keys, values, i);
 
             ShowButtons(keys, values, i);
 
@@ -97,5 +100,105 @@ class SerializableDictionaryEditor : PropertyDrawer
 
         EditorGUI.indentLevel -= 1;
         EditorGUI.EndProperty();
-	}
+    }
+
+
+    protected void DropDown(SerializedProperty list, UnityEngine.Object[] _implementations, List<string> used, int i)
+    {
+        string[] names = _implementations.Select(impl => impl.name).ToArray();
+        string oname = list.GetArrayElementAtIndex(i).objectReferenceValue.name;
+        int _implementationTypeIndex = 0;
+
+        for (int x = 0; x < names.Length; x++)
+        {
+            if (names[x] == oname)
+                _implementationTypeIndex = x;
+        }
+
+        int temp_implementationTypeIndex = EditorGUILayout.Popup(GUIContent.none,
+            _implementationTypeIndex, names, GUILayout.MinWidth(120f));
+
+        string str = _implementations[temp_implementationTypeIndex].name;
+        if (!used.Contains(str) || list.GetArrayElementAtIndex(i).objectReferenceValue.name == str)
+        {
+            _implementationTypeIndex = temp_implementationTypeIndex;
+            list.GetArrayElementAtIndex(i).objectReferenceValue = _implementations[_implementationTypeIndex];
+        }
+    }
+
+    protected virtual string searchPathKey() { return ""; }
+    protected virtual string searchPathValue() { return ""; }
+    protected abstract void ListField(SerializedProperty keys, SerializedProperty values, int i);
+}
+
+[CustomPropertyDrawer(typeof(SerializableDictionary<PickUpInteractable, int>))]
+class SerializableDictionaryEditor_PUI_int : SerializableDictionaryEditor
+{
+    override protected string searchPathKey()
+    {
+        return "Prefabs";
+    }
+
+    override protected void ListField(SerializedProperty keys, SerializedProperty values, int i)
+    {
+        EditorGUILayout.PropertyField(values.GetArrayElementAtIndex(i), GUIContent.none, GUILayout.Width(40f));
+
+        EditorGUILayout.LabelField("x", GUILayout.Width(30f));
+
+        DropDown(keys, _implementations_key, used_keys, i);
+    }
+}
+
+[CustomPropertyDrawer(typeof(SerializableDictionary<Quest, bool>))]
+class SerializableDictionaryEditor_Q_bool : SerializableDictionaryEditor
+{
+    override protected string searchPathKey()
+    {
+        return "Quests";
+    }
+
+    override protected void ListField(SerializedProperty keys, SerializedProperty values, int i)
+    {
+        DropDown(keys, _implementations_key, used_keys, i);
+
+        EditorGUILayout.PropertyField(values.GetArrayElementAtIndex(i), GUIContent.none, GUILayout.Width(40f));
+    }
+}
+
+[CustomEditor(typeof(QuestManager))]
+class QMEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
+
+[CustomPropertyDrawer(typeof(SerializableDictionary<Room, PortalComponent>))]
+class SerializableDictionaryEditor_R_PC : SerializableDictionaryEditor
+{
+    override protected string searchPathKey()
+    {
+        return "Rooms";
+    }
+
+    override protected string searchPathValue()
+    {
+        return "Portals";
+    }
+
+    override protected void ListField(SerializedProperty keys, SerializedProperty values, int i)
+    {
+        DropDown(keys, _implementations_key, used_keys, i);
+        DropDown(values, _implementations_value, used_values, i);
+    }
+}
+
+[CustomEditor(typeof(KeyInteractable))]
+class KeyEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
 }
