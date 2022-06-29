07f3025359aff93779912779b819ed7184971ce5
DasPriester <michelrothboeck@gmail.com>
2022-06-29 16:28:54 +0200

    New Task

---
+++ Assets/Scripts/Managers/Tasks/ObjectWithTagExist.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class ObjectWithTagExist : Task
+{
+    [SerializeField] private string tag;
+
+    public ObjectWithTagExist()
+    {
+        tag = "";
+    }
+    public ObjectWithTagExist(string tag)
+    {
+        this.tag = tag;
+    }
+
+    override public float Done()
+    {
+        return GameObject.FindGameObjectsWithTag(tag).Length;
+    }
+}
