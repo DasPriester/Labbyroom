d317a986750ad60dd47830da3192b02188ff783d
DasPriester <michelrothboeck@gmail.com>
2022-06-29 21:32:29 +0200

    Anvil

--- Assets/Scripts/Interactables/ForgeInteractable.cs
+++ Assets/Scripts/Interactables/ForgeInteractable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ForgeInteractable : Interactable
+public class ForgeInteractable : PickUpInteractable
 {
     private InventoryMenu inventoryMenu;
     
