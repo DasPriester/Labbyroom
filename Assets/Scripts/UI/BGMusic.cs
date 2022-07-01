347c8437a8df4f0498e5f3d225d73eb780ce7957
Niklas267 <unrau73.wu@gmail.com>
2022-07-01 13:46:44 +0200

    almost done

--- Assets/Scripts/UI/BGMusic.cs
+++ Assets/Scripts/UI/BGMusic.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class BGMusic : MonoBehaviour
 {
    public AudioClip musicClip;
+  [SerializeField] private AudioMixer audioMixer = default;
 
-    public void Awake()
+    public void Start()
     {
+        Settings settings = Resources.Load<Settings>("Settings/Current");
+        audioMixer.SetFloat("Master Volume", Mathf.Log10(settings.masterVolume) * 20);
+        audioMixer.SetFloat("Effects Volume", Mathf.Log10(settings.effectsVolume) * 20);
+        audioMixer.SetFloat("Music Volume", Mathf.Log10(settings.musicVolume) * 20);
         transform.GetComponent<AudioSource>().clip = musicClip;
         transform.GetComponent<AudioSource>().Play();
     }
